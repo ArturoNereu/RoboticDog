42fbccf399068b41bdbde29048f6d26a565404f8
Arturo Nereu <hdgam3r@gmail.com>
2019-09-18 15:19:44 -0500

    Added Sprite Switcher Script

---
+++ Assets/Scripts/DamageSwapper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
+
+public class DamageSwapper : MonoBehaviour
+{
+    public SpriteResolver[] spriteResolvers;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteResolvers = transform.Find("robot").GetComponentsInChildren<SpriteResolver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            SwapToDamaged();
+        }
+        else if(Input.GetKeyDown(KeyCode.N))
+        {
+            SwapToNormal();
+        }
+    }
+
+    void SwapToDamaged()
+    {
+        foreach(SpriteResolver resolver in spriteResolvers)
+        {
+            resolver.SetCategoryAndLabel(resolver.name, "Damaged");
+        }
+    }
+
+    void SwapToNormal()
+    {
+        foreach (SpriteResolver resolver in spriteResolvers)
+        {
+            resolver.SetCategoryAndLabel(resolver.name, "Normal");
+        }
+    }
+}
