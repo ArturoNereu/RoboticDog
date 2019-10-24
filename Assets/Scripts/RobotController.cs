d8f495e3a65645a706893c2dd250e1975e98dc3c
Arturo Nereu <hdgam3r@gmail.com>
2019-10-24 16:29:39 -0500

    Switched to Universal RP

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RobotController : MonoBehaviour
 {
+    Vector3 m_MovementDirection;
+
+    List<Transform> m_LegIKTargets;
+
     // Start is called before the first frame update
     void Start()
     {
